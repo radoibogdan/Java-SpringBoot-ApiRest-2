9dbae37869b97e684e7417aea3d05d34964017eb
YBogdan RADOI <radoi.office@gmail.com>
2022-07-13 15:09:47 +0200

    Initial commit - start project

---
+++ src/main/java/com/packt/restwebservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.packt.restwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
